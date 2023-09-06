a14a84e6fcda54913fcaed27ab4df1c22ac2d093
Amit.Salim@global-e.com <Amit.Salim@global-e.com>
2023-09-06 18:17:53 +0300

    update todo component

--- src/components/todo.tsx
+++ src/components/todo.tsx
@@ -1,40 +1,80 @@
-import React from "react";
-import { deleteTodo, updateTodo } from './apiService'; 
+import React, { useEffect, useState } from "react";
+import { deleteTodo, updateTodo } from "./apiService";
 
 export type TodoProps = {
-    userId?: number,
-    id?: string,
-    title?: string,
-    completed?: boolean
-}
+  userId?: number;
+  id?: string;
+  title?: string;
+  completed?: boolean;
+};
 
 export function Todo({ title, id, completed }: TodoProps) {
-    const removeTodo = (id: string) => {
-        deleteTodo(id)
-        .then(data => {
-            console.log("Deleted: ", data);
-        })
-        .catch(err => {
-            console.log("Failed to delete todo with id ", id, ", error:", err); 
-        })
-    }
-    const update = (checked: boolean) => {
-        updateTodo({id: id, completed: checked, title: title})
-        .then(data => {
-            console.log("Todo updated", data);
-        })
-        .catch(err => {
-            console.log("Failed to update todo with id: ", id, ", error: ", err);
-        })
-    }
-    return (
-        <li>
-            <div className="view">
-                <input className="toggle" type="checkbox" onClick={(event) => update(event.currentTarget.checked)} checked={completed}/>
-                <label>{title}</label>
-                <button className="destroy" id={id} onClick={(event) => removeTodo(event.currentTarget.id)}/>
-            </div>
-            <input className="edit" />
-        </li>
-    )
+  const [text, setText] = useState(title);
+  const [isEdit, setIsEdit] = useState(false);
+  const [isChecked, setIsChecked] = useState(completed);
+
+  const removeTodo = (id: string) => {
+    deleteTodo(id)
+      .then((data) => {
+        console.log("Deleted: ", data);
+      })
+      .catch((err) => {
+        console.log("Failed to delete todo with id ", id, ", error:", err);
+      });
+  };
+
+  const updateCheckbox = () => {
+    setIsChecked((prev) => !prev);
+  };
+
+  const editElement = () => {
+    setIsEdit((prev) => !prev);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.currentTarget.value);
+  };
+
+  const handleInputBlur = () => {
+    setIsEdit(false);
+  };
+
+//   const updateTodoElement = (todo: TodoProps) => {
+//     updateTodo(todo)
+//     .then((data) => {
+//       console.log("Todo updated", data);
+//     })
+//     .catch((err) => {
+//       console.log("Failed to update todo with id: ", id, ", error: ", err);
+//     });
+//   }
+
+  return (
+    <li>
+      <div className="view">
+        <input
+          className="toggle"
+          type="checkbox"
+          onChange={updateCheckbox}
+          checked={isChecked}
+        />
+        {isEdit ? (
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => handleInputChange(e)}
+            onBlur={handleInputBlur}
+            autoFocus
+          />
+        ) : (
+          <label onClick={editElement}>{text}</label>
+        )}
+        <button
+          className="destroy"
+          id={id}
+          onClick={(event) => removeTodo(event.currentTarget.id)}
+        />
+      </div>
+    </li>
+  );
 }
